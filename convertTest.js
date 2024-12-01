e2178c26a70cdfb59f61cc81d30cb002703028fd
Harsat <121182309+HarsatMKad@users.noreply.github.com>
2024-12-01 14:46:27 +0700

    fix zip convert

--- convertTest.js
+++ convertTest.js
@@ -1,45 +1,70 @@
 const express = require("express");
 const shpwrite = require("@mapbox/shp-write");
-const fs = require("node:fs/promises"); 
+const fs = require("fs");
 const cors = require("cors");
 const JSZip = require("jszip");
 
 const app = express();
-const port = 5000; 
+const port = 5000;
 
 app.use(cors());
 
+function toBuffer(ab) {
+  var buffer = Buffer.alloc(ab.byteLength),
+    view = new Uint8Array(ab);
+  for (var i = 0; i < buffer.length; ++i) {
+    buffer[i] = view[i];
+  }
+  return buffer;
+}
+
+// Маршрут для генерации Shape-файлов и отправки их клиенту
 app.get("/shapefile", async (req, res) => {
   try {
-    const zip = await shpwrite.zip({
-      type: "FeatureCollection",
-      features: [
-        {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: [0, 0],
-          },
-          properties: {
-            name: "Foo",
-          },
-        },
-        {
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: [0, 10],
-          },
-          properties: {
-            name: "Bar",
-          },
-        },
+    var points = [
+      [
+        [
+          [0, 0],
+          [10, 0],
+          [10, 10],
+          [0, 10],
+          [0, 0],
+        ],
       ],
-    });
+    ];
 
-    res.setHeader('Content-Type', 'application/zip');
-    res.setHeader('Content-Disposition', 'attachment; filename=geojson.zip');
-    res.send(zip);
+    const zip = new JSZip();
+
+    function finish(err, files) {
+      zip.file("polygon.shp", files.shp.buffer);
+      zip.file("polygon.shx", files.shx.buffer);
+      zip.file("polygon.dbf", files.dbf.buffer);
+
+      zip
+        .generateAsync({ type: "nodebuffer" })
+        .then((content) => {
+          res.setHeader("Content-Type", "application/zip");
+          res.setHeader(
+            "Content-Disposition",
+            "attachment; filename=data.zip"
+          );
+          res.send(content);
+        })
+        .catch((err) => {
+          console.error("Error generating zip:", err);
+          res.status(500).send("Error generating zip");
+        });
+    }
+
+    shpwrite.write(
+      // feature data
+      [{ id: 0 }],
+      // geometry type
+      "POLYGON",
+      // geometries
+      points,
+      finish
+    );
   } catch (error) {
     console.error("Error creating shapefile:", error);
     res.status(500).send("Error creating shapefile");
